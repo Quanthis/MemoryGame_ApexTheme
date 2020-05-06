b0abf8cacede56a1b41f232957788429a9f14f31
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-05-06 12:30:12 +0200

    --Added new images and image lists for Normal Mode

--- MemoryGame_ApexTheme/Form_NormalMode.Designer.cs
+++ MemoryGame_ApexTheme/Form_NormalMode.Designer.cs
@@ -29,11 +29,162 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_NormalMode));
+            this.Banglore = new System.Windows.Forms.Button();
+            this.Banglore_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Banglore_2 = new System.Windows.Forms.Button();
+            this.Bloodhound_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Caustic_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Lifeline_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Wraith_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Pathfinder_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Crypto_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Gibraltar_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Mirage_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Octane_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Revenant_Images = new System.Windows.Forms.ImageList(this.components);
+            this.Wattson_Images = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // Banglore
+            // 
+            this.Banglore.BackColor = System.Drawing.Color.Black;
+            this.Banglore.Font = new System.Drawing.Font("Microsoft Sans Serif", 0.1F);
+            this.Banglore.ImageIndex = 1;
+            this.Banglore.ImageList = this.Banglore_Images;
+            this.Banglore.Location = new System.Drawing.Point(27, 26);
+            this.Banglore.Name = "Banglore";
+            this.Banglore.Size = new System.Drawing.Size(79, 140);
+            this.Banglore.TabIndex = 0;
+            this.Banglore.Text = "Banglore";
+            this.Banglore.UseVisualStyleBackColor = false;
+            // 
+            // Banglore_Images
+            // 
+            this.Banglore_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Banglore_Images.ImageStream")));
+            this.Banglore_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Banglore_Images.Images.SetKeyName(0, "Banglore1.png");
+            this.Banglore_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Banglore_2
+            // 
+            this.Banglore_2.BackColor = System.Drawing.Color.Black;
+            this.Banglore_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 0.1F);
+            this.Banglore_2.ImageIndex = 1;
+            this.Banglore_2.ImageList = this.Banglore_Images;
+            this.Banglore_2.Location = new System.Drawing.Point(27, 193);
+            this.Banglore_2.Name = "Banglore_2";
+            this.Banglore_2.Size = new System.Drawing.Size(79, 140);
+            this.Banglore_2.TabIndex = 1;
+            this.Banglore_2.Text = "Banglore";
+            this.Banglore_2.UseVisualStyleBackColor = false;
+            // 
+            // Bloodhound_Images
+            // 
+            this.Bloodhound_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Bloodhound_Images.ImageStream")));
+            this.Bloodhound_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Bloodhound_Images.Images.SetKeyName(0, "BloodHound.png");
+            this.Bloodhound_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Caustic_Images
+            // 
+            this.Caustic_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Caustic_Images.ImageStream")));
+            this.Caustic_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Caustic_Images.Images.SetKeyName(0, "Caustic.png");
+            this.Caustic_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Lifeline_Images
+            // 
+            this.Lifeline_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Lifeline_Images.ImageStream")));
+            this.Lifeline_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Lifeline_Images.Images.SetKeyName(0, "Lifeline.png");
+            this.Lifeline_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Wraith_Images
+            // 
+            this.Wraith_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Wraith_Images.ImageStream")));
+            this.Wraith_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Wraith_Images.Images.SetKeyName(0, "Wraith.png");
+            this.Wraith_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Pathfinder_Images
+            // 
+            this.Pathfinder_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Pathfinder_Images.ImageStream")));
+            this.Pathfinder_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Pathfinder_Images.Images.SetKeyName(0, "Pathfinder.png");
+            this.Pathfinder_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Crypto_Images
+            // 
+            this.Crypto_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Crypto_Images.ImageStream")));
+            this.Crypto_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Crypto_Images.Images.SetKeyName(0, "Crypto.png");
+            this.Crypto_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Gibraltar_Images
+            // 
+            this.Gibraltar_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Gibraltar_Images.ImageStream")));
+            this.Gibraltar_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Gibraltar_Images.Images.SetKeyName(0, "Gibraltar.png");
+            this.Gibraltar_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Mirage_Images
+            // 
+            this.Mirage_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Mirage_Images.ImageStream")));
+            this.Mirage_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Mirage_Images.Images.SetKeyName(0, "Mirage.png");
+            this.Mirage_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Octane_Images
+            // 
+            this.Octane_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Octane_Images.ImageStream")));
+            this.Octane_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Octane_Images.Images.SetKeyName(0, "Octane.png");
+            this.Octane_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Revenant_Images
+            // 
+            this.Revenant_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Revenant_Images.ImageStream")));
+            this.Revenant_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Revenant_Images.Images.SetKeyName(0, "Revenant.png");
+            this.Revenant_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Wattson_Images
+            // 
+            this.Wattson_Images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Wattson_Images.ImageStream")));
+            this.Wattson_Images.TransparentColor = System.Drawing.Color.Transparent;
+            this.Wattson_Images.Images.SetKeyName(0, "Wattson.png");
+            this.Wattson_Images.Images.SetKeyName(1, "Covered_Card.PNG");
+            // 
+            // Form_NormalMode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(1584, 862);
+            this.Controls.Add(this.Banglore_2);
+            this.Controls.Add(this.Banglore);
+            this.Name = "Form_NormalMode";
             this.Text = "Form_NormalMode";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Banglore;
+        private System.Windows.Forms.ImageList Banglore_Images;
+        private System.Windows.Forms.Button Banglore_2;
+        private System.Windows.Forms.ImageList Bloodhound_Images;
+        private System.Windows.Forms.ImageList Caustic_Images;
+        private System.Windows.Forms.ImageList Lifeline_Images;
+        private System.Windows.Forms.ImageList Wraith_Images;
+        private System.Windows.Forms.ImageList Pathfinder_Images;
+        private System.Windows.Forms.ImageList Crypto_Images;
+        private System.Windows.Forms.ImageList Gibraltar_Images;
+        private System.Windows.Forms.ImageList Mirage_Images;
+        private System.Windows.Forms.ImageList Octane_Images;
+        private System.Windows.Forms.ImageList Revenant_Images;
+        private System.Windows.Forms.ImageList Wattson_Images;
     }
 }
